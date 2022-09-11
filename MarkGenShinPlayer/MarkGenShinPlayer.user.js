58acda57311b4f1d0c170660bba732afc77adb54
andywang425 <13120989656@163.com>
2022-09-11 21:57:39 +0800

    ✨ feat: Update new script MarkGenShinPlayer

---
+++ MarkGenShinPlayer/MarkGenShinPlayer.user.js
@@ -0,0 +1,217 @@
+// ==UserScript==
+// @name         原神玩家指示器+
+// @namespace    https://github.com/andywang425
+// @version      0.1
+// @description  自动标注评论区中的原神玩家，依据是评论者动态/视频里是否有原神相关内容
+// @author       andywang425
+// @match        *://www.bilibili.com/video/*
+// @icon         https://static.hdslb.com/images/favicon.ico
+// @connect      api.bilibili.com
+// @grant        GM_xmlhttpRequest
+// @grant        GM_addStyle
+// @license      MIT
+// @run-at       document-start
+// ==/UserScript==
+
+
+(function () {
+    const css = `
+    .genshin-icon {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: calc(2 * 10px);
+        transform-origin: center center;
+        transform: scale(0.5);
+        width: 55px;
+        height: 24px;
+        border-radius: 8px;
+        color: var(--text_white);
+        background-color: #4e00ff;
+        cursor: default;
+    }`;
+    let options = {
+        attributes: true,
+        subtree: true
+    }
+    let targetSet = new Set();;
+    let commentObserver = new MutationObserver(async function (mutationsList, observer) {
+        for (const mutation of mutationsList) {
+            if (targetSet.has(mutation.target)) continue;
+            const userInfoElementObj = getUserInfoElement(mutation);
+            if (!userInfoElementObj || (userInfoElementObj.main.length === 0 && userInfoElementObj.sub.length === 0)) continue;
+            const userNameElementObj = getUserNameElement(userInfoElementObj);
+            const uidObj = getUserId(userNameElementObj);
+            await checkGenshin(userInfoElementObj, uidObj, mutation);
+        }
+    })
+
+    const main = async () => {
+        GM_addStyle(css);
+        const comment = await getComment();
+        commentObserver.observe(comment, options);
+    }
+
+    const getComment = async () => {
+        return new Promise(resolve => {
+            let o = new MutationObserver(function (mutationList, observe) {
+                for (const m of mutationList) {
+                    if (m.target.getAttribute('class') === 'comment') {
+                        resolve(m.target);
+                        o.disconnect();
+                    }
+                }
+            })
+            o.observe(document, options);
+        })
+    }
+
+    const checkGenshin = async (userInfoElementObj, uidObj, mutation) => {
+        for (const type in uidObj) {
+            for (let i = 0; i < uidObj[type].length; i++) {
+                const isGenshin = await limitHasGenshin(uidObj[type][i]);
+                if (isGenshin) {
+                    targetSet.add(mutation.target);
+                    setGenshinIcon(userInfoElementObj[type][i]);
+                }
+            }
+        }
+    }
+
+    const setGenshinIcon = (userInfoElement) => {
+        const icon = document.createElement('div');
+        icon.setAttribute('class', 'genshin-icon');
+        icon.textContent = '原友';
+        userInfoElement.append(icon);
+    }
+
+    const getUserInfoElement = (mutation) => {
+        const className = mutation.target.getAttribute('class');
+        if (className === 'reply-item') {
+            return {
+                main: [...mutation.target.querySelectorAll('.user-info')],
+                sub: [...mutation.target.querySelectorAll('.sub-user-info')]
+            }
+        }
+    }
+
+    const getUserNameElement = (userInfoElementObj) => {
+        return {
+            main: userInfoElementObj.main.map(e => e.querySelector('.user-name')),
+            sub: userInfoElementObj.sub.map(e => e.querySelector('.sub-user-name'))
+        }
+    }
+
+    const getUserId = (userInfoElementObj) => {
+        return {
+            main: userInfoElementObj.main.map(e => e.getAttribute('data-user-id')),
+            sub: userInfoElementObj.sub.map(e => e.getAttribute('data-user-id'))
+        }
+    }
+
+    const _hasGenshin = (uid) => {
+        return new Promise(async resolve => {
+            const dynamic = await getDynamic(uid);
+            const video = await getVideo(uid);
+            if (dynamic.response.data.total > 0 || video.response.data.page.count > 0)
+                resolve(true);
+            else
+                resolve(false);
+        })
+    }
+
+    const limit = (func, delay) => {
+        let prev = Date.now();
+        let queue_length = 0;
+        return function () {
+            let context = this;
+            let args = arguments;
+            let now = Date.now();
+            return new Promise(async resolve => {
+                if (now - prev > delay) {
+                    prev = now;
+                    resolve(await func.apply(context, args));
+                }
+                else {
+                    queue_length++;
+                    setTimeout(async () => {
+                        resolve(await func.apply(context, args));
+                        prev = Date.now();
+                        queue_length--;
+                    }, delay * queue_length);
+                }
+            })
+        }
+    }
+
+    const limitHasGenshin = limit(_hasGenshin, 500);
+
+    const getDynamic = async (uid) => {
+        return await GMR({
+            method: 'GET',
+            url: 'https://api.bilibili.com/x/space/dynamic/search',
+            data: {
+                keyword: '原神',
+                pn: 1,
+                ps: 30,
+                mid: uid
+            },
+            headers: {
+                'Referer': `https://space.bilibili.com/${uid}/search/dynamic?keyword=${encodeURIComponent('原神')}`
+            },
+            responseType: 'json'
+        });
+    }
+
+    const getVideo = async (uid) => {
+        return await GMR({
+            method: 'GET',
+            url: 'https://api.bilibili.com/x/space/arc/search',
+            data: {
+                mid: uid,
+                ps: 30,
+                tid: 0,
+                pn: 1,
+                keyword: '原神',
+                order: 'pubdate',
+                jsonp: 'jsonp'
+            },
+            headers: {
+                'Referer': `https://space.bilibili.com/${uid}/search/dynamic?keyword=${encodeURIComponent('原神')}`
+            },
+            responseType: 'json'
+        });
+    }
+
+    const mylog = (...args) => {
+        console.log('[原神玩家指示器+]', ...args);
+    }
+
+    function GMR(config) {
+        return new Promise(resolve => {
+            if (typeof (config.data) === 'object' && !(config.data instanceof FormData) && !(config.data instanceof Blob)) {
+                let params = new URLSearchParams(config.data).toString();
+                config.url = config.url.concat('?', params);
+            }
+            config._ontimeout = config.ontimeout ?? function () { };
+            config._onerror = config.onerror ?? function () { };
+            config._onload = config.onload ?? function () { };
+            config.ontimeout = function (e) {
+                config._ontimeout(e);
+                config.onerror(e)
+            };
+            config.onerror = function (e) {
+                config._onerror(e);
+                mylog('XHR出错', config, e);
+                resolve(undefined);
+            };
+            config.onload = function (res) {
+                config._onload(res);
+                resolve(res);
+            }
+            GM_xmlhttpRequest(config);
+        })
+    }
+
+    main();
+})();
--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 
 Here are some lightweight userscripts (code lines < 1024) written by myself. If a script got more than 1024 lines of code, I will create a separate repository for it.
 
-|       Name       |            Introduction             |                 Install                  |
-| :--------------: | :---------------------------------: | :--------------------------------------: |
+|       Name       |                Introduction                |                 Install                  |
+| :--------------: | :----------------------------------------: | :--------------------------------------: |
 | 智慧树直播课助手 | [README](Zhihuishu-LiveClassHelper#readme) | [INSTALL][zhihuishu-liveclasshelper-raw] |
+| 原神玩家指示器+  |     [README](MarkGenShinPlayer#readme)     |     [INSTALL][markgenshinplayer-raw]     |
 
 ## License
 
 All the scripts are under [MIT License](https://github.com/andywang425/UserScripts/blob/master/LICENSE).
 
 [zhihuishu-liveclasshelper-raw]: https://github.com/andywang425/UserScripts/raw/master/Zhihuishu-LiveClassHelper/Zhihuishu-LiveClassHelper.user.js
+[markgenshinplayer-raw]: https://github.com/andywang425/UserScripts/raw/master/MarkGenShinPlayer/MarkGenShinPlayer.user.js
